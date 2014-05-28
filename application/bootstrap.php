841e743c07a32b0fa88612affe111e9a03fe282c
wangbai <wangbai@yongche.com>
2014-05-28 14:22:09 +0800

    add comment

--- application/bootstrap.php
+++ application/bootstrap.php
@@ -5,6 +5,7 @@ if (file_exists(dirname(__DIR__).'/vendor/autoload.php')) {
     require dirname(__DIR__).'/vendor/autoload.php';
 } 
 
+// Check if composer vendor contain feather-mvc
 if (!file_exists(dirname(__DIR__).'/vendor/wangbai/feather-mvc')) {
     // Ensure library/ is in include_path
     set_include_path(implode(PATH_SEPARATOR, array(
