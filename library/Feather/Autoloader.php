5de9ae6eeeb2ad3f712cb1d4268e1c0e0b8abdaf
wangshaolin <wangshaolin@yongche.com>
2015-01-22 18:20:56 +0800

    modify autoload

--- library/Feather/Autoloader.php
+++ library/Feather/Autoloader.php
@@ -21,13 +21,38 @@ class Autoloader {
     }
 
     public function init() {
-        spl_autoload_register(array($this, "autoload"));
+        spl_autoload_register(array($this, "_autoload"));
         return;
     }
 
-    private function autoload($className) {
+    private $_namespaces = array();
+    public function registerNamespaces(array $arr){
+        foreach($arr as $key => $path){
+            $this->_namespaces[$this->_spacename($key)] = $path;
+        }
+    }
+
+    private function _spacename($name){
+        if($name == str_replace('\\\\', '\\', $name)){
+            return $name;
+        }else{
+            return $this->_spacename($name);
+        }
+    }
+
+    private function _autoload($className) {
         $fileName  = '';
         $namespace = '';
+
+        $className = $this->_spacename($className);
+        foreach($this->_namespaces as $key => $path){
+            $pos = stripos($className, $key);
+            if($pos === 0){
+                $fileName = $path . str_replace('\\', '/', substr($className, strlen($key))) . '.php';
+                include $fileName;
+                return;
+            }
+        }
 
         if ($lastNsPos = strripos($className, '\\')) {
             $namespace = substr($className, 0, $lastNsPos);
