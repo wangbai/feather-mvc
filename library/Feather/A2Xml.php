5d43f927d951c090de582401c6ff4b624d56e408
hanhaoran <hanhaoran@yongche.com>
2015-05-08 11:53:02 +0800

    modify a little bit

--- library/Feather/A2Xml.php
+++ library/Feather/A2Xml.php
@@ -1,7 +1,7 @@
 <?php
 #########################################################################
 # File Name: Feather/A2Xml.php
-# Desc: 
+# Desc: Convert Array To Xml
 # Author: Shaolin Wang
 # mail: wangshaolin@yongche.com
 # Created Time: 2015年05月05日 星期二 17时02分41秒
@@ -9,28 +9,38 @@
 
 namespace Feather;
 class A2Xml {
-    private $i = 0;
-    private $version    = '1.0';
-    private $encoding   = 'UTF-8';
+
+    private $version    = '1.0';      //Version Of Xml
+    private $encoding   = null;       //Character Encoding Of Xml
     private $root       = 'result';
     private $xml        = null;
-    function __construct() {
-        $this->i = 0;
+
+    const DEFAULT_ENCODING = 'UTF-8'; //Default Character Encoding Of Xml
+
+    public function __construct($encoding = self::DEFAULT_ENCODING) {
         $this->xml = new XmlWriter();
+        $this->encoding = $encoding;
     }
-    public function setHeader(){
+
+    public function getXml( $data = [] ){
+        $this->_setHeader();
+        $this->_toXml($data);
+        return $this->_setEnd();
+    }
+
+    private function _setHeader(){
         $this->xml->openMemory();
         $this->xml->startDocument($this->version, $this->encoding);
         $this->xml->startElement($this->root);
-
     }
-    public function setEnd(){
+
+    private function _setEnd(){
         $this->xml->endElement();
         $this->xml->endDocument();
         return $this->xml->outputMemory(true);
     }
 
-    function toXml($data) {
+    private function _toXml($data) {
         foreach($data as $key => $value){
             if(is_numeric($key)) {
                 $key = 'value';
@@ -40,7 +50,7 @@ class A2Xml {
                 continue;
             } else {
                 $this->xml->startElement($key);
-                $this->toXml($value);
+                $this->_toXml($value);
                 $this->xml->endElement();
             }
         }
